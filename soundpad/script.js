947846d7526bb9f62076083931db2f9395370e66
binmite <144014661+binmite@users.noreply.github.com>
2025-10-17 12:51:27 +0300

    Delete soundpad directory

--- soundpad/script.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.pad-button').forEach(button => {
-  button.addEventListener('click', () => {
-    const soundPath = button.dataset.sound;
-    const audio = new Audio(soundPath);
-    audio.play().catch(err => console.error("Ошибка воспроизведения:", err));
-  });
-});
